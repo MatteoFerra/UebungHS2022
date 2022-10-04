b08736c0215790dae29568d0365d97f53722c2a0
MatteoFerra <114218519+MatteoFerra@users.noreply.github.com>
2022-10-04 19:11:31 +0200

    Uebung02

---
+++ Uebung02/Uebung02.js
@@ -0,0 +1,20 @@
+// Aufgabe 1
+
+var L =[];
+for (var i =1; i<100; i+=2){
+    L.push(i);
+
+}
+console.log(L);
+
+
+
+// Aufgabe 2
+
+function wurfelen() {
+    var a = Math.floor(6*Math.random());
+    return a;
+
+}
+a=wurfelen();
+console.log(a);
